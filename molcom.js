e33e33a3155f00a8a00a58d846bf0b5f8f078ed6
Ofir Podoly <ofir.podoly@gmail.com>
2020-05-27 13:28:17 -0700

    changed the code so that it calculates the center of mass

--- molcom.js
+++ molcom.js
@@ -23,6 +23,47 @@ function downloadMolecule(code) {
     if (this.readyState == 4 && this.status == 200) {
         // callback function here
         displayOnPage(this.responseText);
+        
+        let mypdb = this.responseText;
+        let array = mypdb.split("\n");
+        let x_array = []
+        let y_array = []
+        let z_array = []
+        //console.log(array[1]);
+        for (let counter=0; counter <= array.length; counter += 1) {
+            //console.log(array[counter]);
+            if (typeof(array[counter]) != "undefined" && array[counter].startsWith("ATOM")) {
+              x_array.push(array[counter].slice(30, 38))
+              y_array.push(array[counter].slice(38, 46))
+              z_array.push(array[counter].slice(46, 54))
+            }
+            
+        }
+        // console.log(x_array);
+        // console.log(y_array);
+        // console.log(z_array);
+
+
+        xyz_avg = function(array) {
+          let total = 0
+          for (let i = 0; i < array.length; i++) {
+            let newnumber = Number(array[i])
+            total += newnumber;
+         }
+          let av = total / array.length
+          return(av);
+        }
+
+        console.log("X:" + xyz_avg(x_array))
+        console.log("Y:" + xyz_avg(y_array))
+        console.log("Z:" + xyz_avg(z_array))
+        // let total = 0
+        // for (let i = 0; i < x_array.length; i++) {
+        //   let newnumber = Number(x_array[i])
+        //   total += newnumber;
+        // }
+        // let av = total / x_array.length
+        // console.log(av);
     }
   };
 
