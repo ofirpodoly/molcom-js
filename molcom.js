2c917dca1af5ae127dc58d9a394a1d56b96e8652
Ofir Podoly <ofir.podoly@gmail.com>
2020-06-03 13:53:28 -0700

    finished tidying up code with linter

--- molcom.js
+++ molcom.js
@@ -8,29 +8,21 @@
 
 // Changes the content of the result element.
 function displayOnPage(content) {
-    let resultField = document.getElementById('result');
+    let resultField = document.getElementById("result");
     resultField.innerText = content;
 }
-
 // Downloads data from a website.
 // Request is asynchronous: browser keeps waiting until all data is fetched
 // and then it triggers the callback function
-
-let mypdb;
-
 function downloadMolecule(code) {
-  
-  var xhttp = new XMLHttpRequest();
-
+  let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         // callback function here
-        let result = calculateCenterOfMass(this.responseText)
+        let result = calculateCenterOfMass(this.responseText);
         displayOnPage(result);
-        
     }
   };
-
   let url = "https://files.rcsb.org/view/" + code + ".pdb";
   xhttp.open("GET", url, true);
   xhttp.send();
@@ -38,37 +30,33 @@ function downloadMolecule(code) {
 
 // attached to HTML button
 function run() {
-    let inputbox = document.getElementById('input-text');
+    let inputbox = document.getElementById("input-text");
     let pdbcode = inputbox.value;
 
     downloadMolecule(pdbcode);
 }
 
-calculateCenterOfMass = function(pdbfile){
+function calculateCenterOfMass(pdbfile) {
   let array = pdbfile.split("\n");
-  let x_array = []
-  let y_array = []
-  let z_array = []
+  let x_array = [];
+  let y_array = [];
+  let z_array = [];
 
   for (let counter=0; counter <= array.length; counter += 1) {
-    
-    if (typeof(array[counter]) != "undefined" && array[counter].startsWith("ATOM")) {
+    if (typeof(array[counter]) != "undefined" &&
+    array[counter].startsWith("ATOM")) {
       x_array.push(array[counter].slice(30, 38))
       y_array.push(array[counter].slice(38, 46))
       z_array.push(array[counter].slice(46, 54))
     }
-    
   }
-
   let final_x = "X:" + xyz_avg(x_array)
   let final_y = "Y:" + xyz_avg(y_array)
   let final_z = "Z:" + xyz_avg(z_array)
 
   return final_x + ", " + final_y + ", " + final_z
-
 }
-
-xyz_avg = function(array) {
+function xyz_avg(array) {
   let total = 0
   for (let i = 0; i < array.length; i++) {
     let newnumber = Number(array[i])
@@ -77,6 +65,3 @@ xyz_avg = function(array) {
   let av = total / array.length
   return av
 }
-
-        
-      
